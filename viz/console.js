be023a7ec27198f3535c9b7643642c111ce7a780
Andrew Nowak <a.nowak.2769@gmail.com>
2018-03-02 18:36:20 +0000

    QOL for console visualiser
    If stdout is not a tty, run quickly and do not clear output

--- viz/console.js
+++ viz/console.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const util = require('util');
-const log = require('log-update');
 const Game = require('..');
 
+const log = process.stdout.isTTY ? require('log-update') : console.log;
 /**
  * An example visualiser which runs a game in the console output.
  * Shows a new frame every 200ms and ends after 10s.
@@ -11,11 +11,18 @@ const Game = require('..');
 
 let game = new Game();
 
-let interval = setInterval(function() {
-  log(util.format(game.grid));
-  game.iterate();
-}, 200);
+if (process.stdout.isTTY) {
+  let interval = setInterval(function() {
+    log(util.format(game.grid));
+    game.iterate();
+  }, 200);
 
-setTimeout(function(interval) {
-  clearInterval(interval);
-}, 10000, interval);
+  setTimeout(function(interval) {
+    clearInterval(interval);
+  }, 10000, interval);
+} else {
+  for (let i = 0; i < 50; i++) {
+    log(util.format(game.grid));
+    game.iterate();
+  }
+}
